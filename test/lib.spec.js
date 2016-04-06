745bd7aac3a3856ea7a681f088462478c553cdcf
s-a <stephan.ahlf@googlemail.com>
2016-04-06 21:08:47 +0200

    js coverage

--- test/lib.spec.js
+++ test/lib.spec.js
@@ -1,23 +1,29 @@
 "use strict";
 
 var fs = require("fs");
-var path = require("path");
+// var path = require("path");
 require("should");
-var UserAppData = require("./../index.js");
+var UserAppData; 
+
+try {
+	UserAppData = require("./../lib-cov/index.js");
+} catch(e){
+	UserAppData = require("./../lib/index.js");
+}
 
 
 
 describe("UserAppData.uninstall", function(){
 	var config = new UserAppData({appname: "mocha-test-client", filename:"setup.json", defaultSettings: { foo:"bar" }});
 	config.uninstall();
-	it('should not exist a config file', function() {
+	it("should not exist a config file", function() {
 		fs.existsSync(config.filename).should.be.false;
 	});
 });
 
 describe("UserAppData.init without filename", function(){
 	var config = new UserAppData({appname: "mocha-test-client", defaultSettings: { foo:"bar" }});
-	it('should exist a config file', function() {
+	it("should exist a config file", function() {
 		fs.existsSync(config.filename).should.be.true;
 	});
 });
